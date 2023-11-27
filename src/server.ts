fb7373b8e3e3df0a10184a5990e1fdd083ba729f
Leonardo Sarquiz Hugenthobler <leonardosarquiz14@gmail.com>
2023-11-27 16:39:02 -0300

    add product

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 import cors from 'cors'
 import { router } from './routes'
+import path from 'path'
 
 
 const app = express();
@@ -10,6 +11,11 @@ app.use(cors())
 
 
 app.use(router)
+
+app.use(
+  '/files',
+  express.static(path.resolve(__dirname, '..', 'tmp'))
+)
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
 
